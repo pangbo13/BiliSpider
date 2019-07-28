fbb35ebd2b6db94e2c83dfcedc55b4e52a260f6c
pangbo <pangbo13@users.noreply.github.com>
2019-07-28 20:37:18 +0800

    update to 0.1.4

--- BiliSpider/BiliSpider.py
+++ BiliSpider/BiliSpider.py
@@ -4,12 +4,72 @@ import time
 import threading
 import sys
 
+<<<<<<< Updated upstream
+=======
+class spider():
+	
+	#构造函数
+	def __init__(self,rid,config={}):
+		
+		self.set_logger(config)
+>>>>>>> Stashed changes
 
 class Spider():
 	def __init__(self,rid,thread_num=2):
 		self.url = 'https://api.bilibili.com/x/web-interface/newlist?rid={}&pn={}'.format(rid,'{}')
 		self.rid = rid
+<<<<<<< Updated upstream
 		self.thread_num = thread_num
+=======
+		if rid not in config['tid']:
+			self.logger.warning('分区id不一致，请检查设置')
+		self.thread_num = config.get('thread_num',2)
+
+		self.logger.debug("构造完成")
+
+	def set_logger(self,config):
+
+		#配置日志记录
+		FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s '
+		FILENAME = r'./log/'+'-'.join(map(str,tuple(time.localtime())[:5])) + '.log'
+		#logging.basicConfig(level = logging.DEBUG,format = FORMAT ,datefmt='%H:%M:%S')
+		logger = logging.getLogger(__name__)
+		if config.get('debug',False):
+			logger.setLevel(level = logging.DEBUG)
+		elif config.get('logmode',1) ==0 and config.get('output',1) == 0:
+			logger.setLevel(level = logging.FATAL)
+		else:
+			logger.setLevel(level = logging.INFO)
+
+		#配置输出日志文件
+		file_log_level = (0,logging.ERROR,logging.DEBUG)[config.get('logmode',1)]
+		if file_log_level != 0 and config.get('debug',False):
+			handler = logging.FileHandler(FILENAME,encoding='utf-8')
+			handler.setLevel(file_log_level)
+			handler.setFormatter(logging.Formatter(fmt = FORMAT,datefmt='%H:%M:%S'))
+			logger.addHandler(handler)
+
+		#配置控制台日志输出
+		console = logging.StreamHandler()
+		if config.get('output',1) != 1 :
+			console.setLevel(logging.DEBUG)
+		else:
+			console.setLevel(logging.FATAL)
+		console.setFormatter(logging.Formatter(fmt = FORMAT,datefmt='%H:%M:%S'))
+		logger.addHandler(console)
+
+		#配置进度条
+		if config.get('output',1) == 1:
+			self.SHOW_BAR = True
+		else :
+			self.SHOW_BAR = False
+
+		#日志配置完成
+		logger.info("日志配置完毕")
+
+		self.logger = logger
+	
+>>>>>>> Stashed changes
 	#初始化函数
 	def prepare(self):
 		threadLock = threading.Lock()
@@ -23,6 +83,11 @@ class Spider():
 		file = open(filename, 'a+',encoding='utf-8')
 		#输出当前时间
 		file.write(time.ctime(time.time()) + '\n')
+<<<<<<< Updated upstream
+=======
+		#导入请求头
+		from .headers import Api_headers as headers
+>>>>>>> Stashed changes
 		#封装全局变量
 		self.global_var ={
 			'threadLock' : threadLock,
@@ -42,9 +107,14 @@ class Spider():
 			print(all_pages)
 			self.global_var['all_pages'] = all_pages
 		except:
+<<<<<<< Updated upstream
 			print('获取总页数失败')
 			print('服务器返回内容：')
 			print(res.text)
+=======
+			self.logger.error("获取总页数失败",exc_info = True)
+			self.logger.error("服务器返回内容：\n" + res.content.decode('utf-8'))
+>>>>>>> Stashed changes
 			exit()
 	def start(self):
 		# 创建新线程
@@ -62,6 +132,7 @@ class Spider():
 		threads = self.global_var['threads']
 		for t in threads:
 			t.join()
+<<<<<<< Updated upstream
 		
 	#消息处理函数
 	def Info(self,threadname,type,content):
@@ -73,6 +144,19 @@ class Spider():
 		if type == 'DEBUG':
 			print('[{}][DEBUG][{}]{}'.format(INFO_time,threadname,content))	
 			
+=======
+
+	def close(self):
+		self.global_var['file'].close()
+	
+	def auto_run(self):
+		self.prepare()
+		self.start()
+		self.wait()
+		self.close()
+	
+
+>>>>>>> Stashed changes
 	class SpiderThread (threading.Thread):
 		def __init__(self, threadID, name, father):
 			threading.Thread.__init__(self)
@@ -80,16 +164,32 @@ class Spider():
 			self.name = name
 			self.pagesget = 0
 			self.father = father
+<<<<<<< Updated upstream
 			self.father.Info(self.name,'DEBUG','线程已创建！')
 		def run(self): 
 			Info = self.father.Info			#转存Info函数
+=======
+			self.logger = father.logger
+
+			self.logger.info(self.logformat("线程已创建！"))
+		def logformat(self,msg):
+			return self.name + ' - ' + msg
+
+		def run(self): 
+>>>>>>> Stashed changes
 			#转存全局参数
 			var = self.father.global_var
 			all_pages = var['all_pages']
 			url = var['url']
 			f = var['file']
 			threadLock = var['threadLock']
+<<<<<<< Updated upstream
 			Info(self.name,'DEBUG','线程已开始运行！')
+=======
+			logger = self.logger
+			logformat = self.logformat
+			logger.debug(logformat('线程已开始运行！'))
+>>>>>>> Stashed changes
 			time.sleep(0.5)
 			#global all_pages,now_pages,f
 			while 1:
@@ -101,13 +201,21 @@ class Spider():
 				#判断是否继续
 				if (pages>all_pages):
 					break
+<<<<<<< Updated upstream
 				Info(self.name,'INFO','正在处理第{}页'.format(pages))
+=======
+				logger.debug("正在处理第{}页".format(pages))
+>>>>>>> Stashed changes
 				#连接服务器
 				s_time = time.time()*1000
 				try:
 					res = requests.get(url.format(pages),timeout = 2)
 				except:
+<<<<<<< Updated upstream
 					Info(self.name,'ERROR','第{}页连接超时'.format(pages))
+=======
+					logger.error(logformat('第{}页连接超时'.format(pages)))
+>>>>>>> Stashed changes
 					try:
 						time.sleep(2)
 						res = requests.get(url.format(pages),timeout = 10)
@@ -120,19 +228,22 @@ class Spider():
 				request_time =int( e_time - s_time )
 				
 				s_time = time.time()*1000
+				items = ('aid','view','danmaku','reply','favorite','coin','share','like','dislike',)
 				out = ''
 				#解析数据
 				for vinfo in res.json()['data']['archives']:
-					out += 	repr(vinfo['stat']['aid']) + ','
-					out +=  repr(vinfo['stat']['view']) +  ','
-					out +=  repr(vinfo['stat']['danmaku']) +  ','
-					out +=  repr(vinfo['stat']['reply']) +  ','
-					out +=  repr(vinfo['stat']['favorite']) +  ','
-					out +=  repr(vinfo['stat']['coin']) +  ','
-					out +=  repr(vinfo['stat']['share']) +  ','
-					out +=  repr(vinfo['stat']['like']) +  ','
-					out +=  repr(vinfo['stat']['dislike']) +  '\n'
+					out += ','.join(map(str,[vinfo['stat'][item] for item in items ])) + '\n'
+					# out += 	repr(vinfo['stat']['aid']) + ','
+					# out +=  repr(vinfo['stat']['view']) +  ','
+					# out +=  repr(vinfo['stat']['danmaku']) +  ','
+					# out +=  repr(vinfo['stat']['reply']) +  ','
+					# out +=  repr(vinfo['stat']['favorite']) +  ','
+					# out +=  repr(vinfo['stat']['coin']) +  ','
+					# out +=  repr(vinfo['stat']['share']) +  ','
+					# out +=  repr(vinfo['stat']['like']) +  ','
+					# out +=  repr(vinfo['stat']['dislike']) +  '\n'
 				#写入数据
+<<<<<<< Updated upstream
 				threadLock.acquire()
 				f.write(out)
 				threadLock.release()
@@ -140,6 +251,12 @@ class Spider():
 				write_time =int( e_time - s_time )
 				
 				Info(self.name,'DEBUG','第{}页-{}ms,{}ms'.format(pages,request_time,write_time))
+=======
+				queue.put(out,block=False)
+				e_time = time.time()*1000
+				write_time =int( e_time - s_time )
+				logger.debug(logformat('第{}页-{}ms,{}ms'.format(pages,request_time,write_time)))
+>>>>>>> Stashed changes
 				time.sleep(0.2)
 				self.pagesget += 1
 
@@ -149,9 +266,11 @@ class Spider():
 			self.threadID = threadID
 			self.name = name
 			self.father = father
-			self.father.Info(self.name,'DEBUG','线程已创建！')
+			self.logger = father.logger
+			self.logger.debug(self.logformat('线程已创建！'))
 		def run(self):
 			var = self.father.global_var
+<<<<<<< Updated upstream
 			threads = var['threads']
 			#global threads,IF_finish
 			
@@ -172,3 +291,25 @@ spider1.prepare()
 spider1.start()
 spider1.wait()
 spider1.global_var['file'].close()
+=======
+			queue = var['queue']
+			f = var['file']
+			threads = var['threads'][1:]
+			monitor_output = self.show_bar
+			time.sleep(1)
+			while bool(sum(t.isAlive() for t in threads)):
+				if self.father.SHOW_BAR :
+					percentage = (var['now_pages']-1)/var['all_pages']
+					monitor_output(percentage,BAR_LENGTH)
+				time.sleep(0.5)
+				while not queue.empty():
+					f.write(queue.get(block=False))
+			monitor_output(1,BAR_LENGTH)
+
+		def logformat(self,msg):
+			return self.name + ' - ' + msg
+
+		def show_bar(self,percentage,BAR_LENGTH):
+			count = int(percentage*BAR_LENGTH)
+			print('\r[{}{}] --{}%   '.format('#' * count ,' ' * (BAR_LENGTH - count),round(percentage*100,2)),end = '')
+>>>>>>> Stashed changes
